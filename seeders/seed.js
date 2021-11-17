fd1006f8f613942469c980305a75b88a385bcad9
Garrison <79775636+segarrison@users.noreply.github.com>
2021-11-17 09:44:46 -0600

    trying to fix heroku seeder issue

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models/workout");
 
-mongoose.connect("mongodb://localhost/workoutDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
